600b24b5fc4491a80717ff838b2238194ecdc4e3
sharifhabiman <hdidiersharif@gmail.com>
2021-12-13 22:36:59 +0200

    commiting again

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,53 +1,24 @@
 #!/usr/bin/python3
-
 """
-Module containing class Rectangle
+Defines a class Rectangle
 """
 
 
 class Rectangle:
-    """
-    A rectangle that has a width and height. Both are 0 by default.
-    """
-
+    """Representation of a rectangle"""
     def __init__(self, width=0, height=0):
-        """
-        width and height are initialized here. Appropriate error is printed if
-        passed vars are incorrect
-        Args:
-            width (int): width of the rectangle
-            height (int): height of the rectangle
-        Attributes:
-            width (int): width of rectangle, initialized by constructor
-            height (int): height of rectangle, initialized by constructor
-        """
-        if type(width) is not int:
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = width
-
-        if type(height) is not int:
-            raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = height
+        """Initializes the rectangle"""
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
-        """Get private attribute width
-        Returns:
-            The attribute width
-        """
+        """getter for the private instance attribute width"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Sets the private attribute width to value, or raises an exception
-        if value doesn't meet the requirements
-        Args:
-            value (int): the new value of width
-        """
+        """setter for the private instance attribute width"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -56,19 +27,12 @@ class Rectangle:
 
     @property
     def height(self):
-        """Get private attribute height
-        Returns:
-            The attribute height
-        """
+        """getter for the private instance attribute height"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Sets the private attribute height to value, or raises an exception
-        if value doesn't meet the requirements
-        Args:
-            value (int): the new value of height
-        """
+        """setter for the private instance attribute height"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
