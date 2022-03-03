5f060c3a0de28684f070e3b1c7b2804c6edd9ee4
sharifhabiman <hdidiersharif@gmail.com>
2022-03-03 13:36:31 +0200

    semistandard

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const arr = ['C is fun','Python is coll','JavaScript is amazing']
-arr.forEach((value)=>{
-    console.log(value)
-})
+const arr = ['C is fun', 'Python is coll', 'JavaScript is amazing'];
+arr.forEach((value) => {
+  console.log(value);
+});
