b8ad3e206c2035218c3b77cca11c054f674ab72d
sharifhabiman <hdidiersharif@gmail.com>
2022-03-08 15:29:27 +0200

    correct 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -16,10 +16,8 @@ def main():
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
     c = db.cursor()
-    c.execute("SELECT * FROM states WHERE name REGEXP '^N' ORDER BY id")
-    for i in c.fetchall():
-        print(i)
-
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in c.fetchall() if state[1][0] == "N"]
 
 if __name__ == "__main__":
     main()
