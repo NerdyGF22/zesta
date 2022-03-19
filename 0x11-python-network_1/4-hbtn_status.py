569904e0dd19f77f5cf43e5daa5388a6f709cc53
sharifhabiman <hdidiersharif@gmail.com>
2022-03-19 18:03:34 +0200

    fetch using requests

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,19 +1,12 @@
-#!/usr/bin/python3
-"""
-- script that fetches https://alx-intranet.hbtn.io/status
-- displays the body of the response 
-- decoded in utf-8
 
-"""
+#!/usr/bin/python3
+"""fetches https://intranet.hbtn.io/status."""
 import requests
 
 
-def main():
-   req = requests.get("https://intranet.hbtn.io/status")
-   print("Body response:")
-   print("\t- type: {}".format(type(req.text)))
-   print("\t- content: {}".format(req.text))
-
-
-if "__name__" == "__main__":
-    main()
+if __name__ == "__main__":
+    r = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
+    
