d279a642a8087fae0cb26f54ebc3da236520e5ea
sharifhabiman <hdidiersharif@gmail.com>
2022-03-19 18:59:57 +0200

    githubId

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+- Python script that takes your GitHub credentials
+- uses the GitHub API to display your id
+"""
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+def main():
+    """ main"""
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
+
+
+if __name__ == "__main__":
+    main()
