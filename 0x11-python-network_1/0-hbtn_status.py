880ed7dceece0ba26031f146ceaf10e1b51e228b
sharifhabiman <hdidiersharif@gmail.com>
2022-03-19 13:02:38 +0200

    FETCH INTRANET

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,8 +10,9 @@ def main():
     """ fetch python script"""
     with urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
-        print('Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 \
-        content: {}'.format(type(html), html, html.decode('utf-8')))
+        print('Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8'
+        .format(type(html),html),
+        'content: {}'.format(html.decode('utf-8')))
 
 
 if __name__ == "__main__":
