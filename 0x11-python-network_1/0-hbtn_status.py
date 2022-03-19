e037d9c420899cde87aac7d676047e4074c6f941
sharifhabiman <hdidiersharif@gmail.com>
2022-03-19 12:52:25 +0200

    fetch intranet

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,6 +3,16 @@
 Write a Python script that fetches https://alx-intranet.hbtn.io/status
 """
 
-import urllib.request
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    html = response.read()
+from urllib.request import urlopen
+
+
+def main():
+    """ fetch python script"""
+    with urlopen('https://alx-intranet.hbtn.io/status') as response:
+        html = response.read()
+        print('Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 \
+        content: {}'.format(type(html), html, html.decode('utf-8')))
+
+
+if __name__ == "__main__":
+    main()
