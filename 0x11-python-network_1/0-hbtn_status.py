d01c79459a483213079596d9f514a24881fc1801
sharifhabiman <hdidiersharif@gmail.com>
2022-03-19 17:46:49 +0200

    pycodestyle

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,10 +9,11 @@ from urllib.request import urlopen
 def main():
     """ fetch python script"""
     with urlopen('https://alx-intranet.hbtn.io/status') as response:
-        html = response.read()
-        print('Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8'
-        .format(type(html), html),
-        'content: {}'.format(html.decode('utf-8')))
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
 
 
 if __name__ == "__main__":
