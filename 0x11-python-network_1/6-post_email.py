528c7e3cb36551e347f282c3c4292d2affc79910
sharifhabiman <hdidiersharif@gmail.com>
2022-03-19 18:26:02 +0200

    post using requests

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+"""
+-Python script that takes in a URL and an email address
+-sends a POST request to the passed URL with the email
+-as a parameter,
+-and finally displays the body of the response.
+"""
+import sys
+import requests
+
+
+def main():
+    """ use request to post email"""
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+
+    r = requests.post(url, data=value)
+    print(r.text)
+
+
+if __name__ == "__main__":
+    main()
