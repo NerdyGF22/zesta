ec49bf8bbb7686979fdb1089ffc629cd457944bb
sharifhabiman <hdidiersharif@gmail.com>
2022-04-15 15:30:29 +0200

    star wars count

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,11 +2,11 @@
 const request = require('request');
 const id = (process.argv)[2];
 
-request(`https://swapi-api.hbtn.io/api/films/${id}`,(error,response,body)=>{
-    if(error){
-        console.log(error);
-        return;
-    }
-    const movie= JSON.parse(body)
-    console.log(movie.title)
-})
+request(`https://swapi-api.hbtn.io/api/films/${id}`, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const movie = JSON.parse(body);
+  console.log(movie.title);
+});
---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const request = require('request');
+const url = (process.argv)[2];
+request(url,(err,res,body)=>{
+    if(err){
+        console.log(err);
+        return;
+    }
+    
+    const obj=JSON.parse(body);
+    const films =obj.results;
+    let count=0;
+    for(let j=0 ; j < films.length; j++){
+        const film= films[j];
+        const characters =film.characters
+        for(i=0;i<characters.length;i++){
+            if((characters[i]).includes('18')){
+                count++
+
+            }
+        }
+    }
+console.log(count)
+})
