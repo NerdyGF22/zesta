a50f68eea5f3410c97942bb15e9730ded4502d16
sharifhabiman <hdidiersharif@gmail.com>
2022-04-15 14:57:48 +0200

    starwars tittle

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const request = require('request');
+const id = (process.argv)[2];
+
+request(`https://swapi-api.hbtn.io/api/films/${id}`,(error,response,body)=>{
+    if(error){
+        console.log(error);
+        return;
+    }
+    const movie= JSON.parse(body)
+    console.log(movie.title)
+})
