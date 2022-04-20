2c20fb34446d0da1c249fb76ae7c6647beb7f8b5
sharifhabiman <hdidiersharif@gmail.com>
2022-04-20 22:04:25 +0200

    add class

--- 0x15-javascript-web_jquery/3-script.js
+++ 0x15-javascript-web_jquery/3-script.js
@@ -1,5 +1,7 @@
-$(document),ready(function(){
-    $('#red_header').on('click', function(){
-        $('header').addClass('red')
-    })
-})
+// Turns the header red when the 'red header' text is clicked on
+// by setting the header's class to red
+$(document).ready(function () {
+    $('#red_header').on('click', function () {
+      $('header').addClass('red');
+    });
+  });
