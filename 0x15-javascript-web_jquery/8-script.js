1a78b706b8a553dc6706918c463b6c4d0877a815
sharifhabiman <hdidiersharif@gmail.com>
2022-04-21 13:29:56 +0200

    salute

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
-    $.ajax({
-       type:'GET',
-       url:'https://swapi-api.hbtn.io/api/films/?format=json' ,
-       success: function(data){
-           for(const film in data.results){
-               $('#list_movies').append(`<li> ${data.results[film].title}</li>`)
-           }
-       }
-    })
-})
+$(document).ready(function () {
+  $.ajax({
+    type: "GET",
+    url: "https://swapi-api.hbtn.io/api/films/?format=json",
+    success: function (data) {
+      for (const film in data.results) {
+        $("#list_movies").append(`<li> ${data.results[film].title}</li>`);
+      }
+    },
+  });
+});
